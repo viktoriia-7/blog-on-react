4d521eb8466627e5681c262511514f9f1e0e08bd
Viktoriia <viktoriiafesyk@gmail.com>
2023-06-20 17:50:15 +0300

    installed swiper

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -1,9 +1,49 @@
-import './Header.scss' 
+import './Header.scss'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import MenuIcon from '@mui/icons-material/Menu'
+import SearchIcon from '@mui/icons-material/Search'
+import { Container } from '@mui/material'
 
 type Props = {}
 const Header = (props: Props) => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <AppBar
+            position="static"
+            style={{ backgroundColor: 'white', color: 'black' }}
+        >
+            <Container>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'none', sm: 'block' },
+                        }}
+                    ></Typography>
+                    <IconButton
+                        size="large"
+                        aria-label="search"
+                        color="inherit"
+                    >
+                        <SearchIcon />
+                    </IconButton>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
 }
 export default Header
--- src/Container/Main/Main.tsx
+++ src/Container/Main/Main.tsx
@@ -1,7 +1,16 @@
+import Logo from 'components/Logo/Logo'
+import PostList from 'components/Post/PostList'
+import { Container } from '@mui/material'
+import Slider from 'components/Slider/Slider'
+
 type Props = {}
 const Main = (props: Props) => {
-  return (
-    <div>Main</div>
-  )
+    return (
+        <Container>
+            <Logo />
+            <Slider />
+            <PostList />
+        </Container>
+    )
 }
 export default Main
