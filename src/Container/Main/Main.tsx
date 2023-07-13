577c1b891fdd46359b5b8a738b3f1f840ded0ef2
Viktoriia <viktoriiafesyk@gmail.com>
2023-07-13 15:11:18 +0300

    added SubscribeForm

--- src/Container/Main/Main.tsx
+++ src/Container/Main/Main.tsx
@@ -2,6 +2,7 @@ import Logo from 'components/Logo/Logo'
 import PostList from 'components/Post/PostList'
 import { Container } from '@mui/material'
 import Slider from 'components/Slider/Slider'
+import SubscribeForm from 'components/Subscribe/SubscribeForm'
 
 type Props = {}
 const Main = (props: Props) => {
@@ -9,6 +10,7 @@ const Main = (props: Props) => {
         <Container>
             <Logo />
             <Slider />
+            <SubscribeForm/>
             <PostList />
         </Container>
     )
