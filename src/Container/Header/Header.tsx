4c7d1dabdc2274ce06dcf7f5a5a41f0c56e1bde0
Viktoriia <viktoriiafesyk@gmail.com>
2023-07-15 02:21:31 +0300

    added routes, pages, Menu

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -6,44 +6,51 @@ import Typography from '@mui/material/Typography'
 import MenuIcon from '@mui/icons-material/Menu'
 import SearchIcon from '@mui/icons-material/Search'
 import { Container } from '@mui/material'
+import Menu from 'components/Menu/Menu'
 
 type Props = {}
 const Header = (props: Props) => {
     return (
-        <AppBar
-            position="static"
-            style={{ backgroundColor: 'white', color: 'black' }}
-        >
-            <Container>
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: 'none', sm: 'block' },
-                        }}
-                    ></Typography>
-                    <IconButton
-                        size="large"
-                        aria-label="search"
-                        color="inherit"
-                    >
-                        <SearchIcon />
-                    </IconButton>
-                </Toolbar>
-            </Container>
-        </AppBar>
+        <Container>
+             <Menu />
+            <div className="header-wrap"></div>
+        </Container>
+        // <AppBar
+        //     position="static"
+        //     style={{ backgroundColor: 'white', color: 'black' }}
+        // >
+        //     <Container>
+        //         <Toolbar>
+        //             <IconButton
+        //                 size="large"
+        //                 edge="start"
+        //                 color="inherit"
+        //                 aria-label="open drawer"
+        //                 sx={{ mr: 2 }}
+        //             >
+        //                 <MenuIcon />
+        //             </IconButton>
+        //             <Menu />
+        //             <Typography
+        //                 variant="h6"
+        //                 noWrap
+        //                 component="div"
+        //                 sx={{
+        //                     flexGrow: 1,
+        //                     display: { xs: 'none', sm: 'block' },
+        //                 }}
+        //             ></Typography>
+
+        //             <IconButton
+        //                 size="large"
+        //                 aria-label="search"
+        //                 color="inherit"
+        //             >
+        //                 <SearchIcon />
+        //             </IconButton>
+        //         </Toolbar>
+        //     </Container>
+        // </AppBar>
     )
 }
 export default Header
