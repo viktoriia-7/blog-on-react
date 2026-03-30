e43b2019d6d263558b29a553b8f3d2bb1cf0b503
Viktoriia <viktoriiafesyk@gmail.com>
2026-03-30 15:15:13 +0300

    added changed in routes

--- src/routes/FashionListRoute.tsx
+++ src/routes/FashionListRoute.tsx
@@ -1,7 +1,6 @@
 import Header from 'Container/Header/Header'
-import FashionPage from 'pages/FashionPage'
 import { useLoaderData } from 'react-router-dom'
-import FashionPageList, { listFashionPost} from 'pages/FashionPageList'
+import FashionPageList, { listFashionPost } from 'pages/FashionPageList'
 import { FashionPost } from 'pages/FashionPage'
 
 export async function loader({ params }: any) {
