cb6314fa84dd449f31d3bb63168d48aaedea771d
Viktoriia <viktoriiafesyk@gmail.com>
2026-03-30 21:11:13 +0300

    added Like

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'Container/App/App'
+import { Provider } from 'react-redux'
+import { store } from 'assets/redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
+        <Provider store={store}>
             <App />
+        </Provider>
     </React.StrictMode>
 )
