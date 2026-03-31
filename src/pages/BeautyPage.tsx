78ffb5696164fe84bab9fcf313e1c91b03b970fe
Viktoriia <viktoriiafesyk@gmail.com>
2026-03-31 14:13:13 +0300

    added BeautyPage

--- src/pages/BeautyPage.tsx
+++ src/pages/BeautyPage.tsx
@@ -1,43 +1,27 @@
 import { Container } from '@mui/material'
-import image1 from 'assets/posts-images/beauty_1.jpg'
+import beautyPostsArray from 'utils/beautyPostsArray'
 
-const BeautyPage = () => {
+export interface BeautyPost {
+    id: string
+    title: string
+    imageUrl: string
+    content: string
+}
+
+interface Props {
+    post: BeautyPost
+}
+
+export function getBeautyPost(postId: string): BeautyPost | undefined {
+    return beautyPostsArray.find((post) => post.id === postId)
+}
+const BeautyPage = ({ post }: Props) => {
     return (
         <Container>
             <h1>Beauty</h1>
-            <h2>Exploring Beauty Trends</h2>
-            <img src={image1} alt="Exploring Beauty Trends"></img>
-            <p>
-                The world of beauty is ever-evolving, with new trends emerging
-                and captivating our attention. From innovative makeup techniques
-                to bold hairstyles, there's always something new to explore and
-                experiment with. Beauty trends allow us to express our
-                creativity and embrace our individuality. They serve as a canvas
-                for self-expression, empowering us to try new looks and step out
-                of our comfort zones. Whether it's the latest lip color or a
-                cutting-edge hairstyle, beauty trends provide an exciting
-                playground for us to discover and redefine our personal style.
-                Makeup, in particular, offers an endless array of trends to
-                explore. From the "no-makeup" look that focuses on enhancing
-                natural features to vibrant and experimental looks that push
-                boundaries, there is something for everyone. Whether you prefer
-                a subtle and dewy glow or a bold and dramatic statement, makeup
-                trends enable us to transform ourselves and have fun with our
-                appearance. Hairstyles are another avenue where trends come to
-                life. From pixie cuts to long beachy waves, there's a hairstyle
-                trend to suit every personality and occasion. The beauty
-                industry constantly introduces new techniques and products that
-                allow us to achieve any desired look, from intricate braids to
-                sleek updos. Hairstyle trends offer us the opportunity to
-                reinvent ourselves, experiment with different textures, and
-                express our individuality. While beauty trends can be exciting
-                to follow, it's essential to remember that true beauty is not
-                solely defined by what's popular at the moment. It's important
-                to stay true to yourself and embrace trends that resonate with
-                your personal style and make you feel confident and comfortable.
-                After all, beauty trends may come and go, but embracing your
-                unique beauty is timeless.`
-            </p>
+            <h2>{post.title}</h2>
+            <img src={post.imageUrl} alt="Exploring Beauty Trends"></img>
+            <p>{post.content}</p>
         </Container>
     )
 }
